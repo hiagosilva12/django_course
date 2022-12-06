bb11ce361b261d0ed1ca165afad63da3ca64f326
Dev Hiago <hiagodasilvaconceicao@gmail.com>
2022-12-06 19:34:44 -0300

    chore: update seetings

--- setup/settings.py
+++ setup/settings.py
@@ -114,7 +114,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'https://escola-hiago.netlify.app',
-]
+CORS_ALLOWED_ORIGINS = ["*"]
